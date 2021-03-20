df87dd5a4724ccc7f6f4f1811013f2378f6b3702
Tonmoy dey <tonmoyd2001@gmail.com>
2021-03-21 00:23:04 +0600

    my comment

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        double b = 10.00;
+        float c =13.0f;
+        long d =235L;
+        char e = 'A';
+        Boolean f = true;
+        byte g = 3;
+        String h = ("tonmoy");
+        String i = new String("ton");
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+    }
+}
