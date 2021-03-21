610acdc2e781afe6e62d099013ad067cf9b7b7ba
Tonmoy dey <tonmoyd2001@gmail.com>
2021-03-21 23:06:30 +0600

    my program

---
+++ size.java
@@ -0,0 +1,57 @@
+public class size {
+
+    public static void main(String[] args) {
+
+        byte byteMax = Byte.MAX_VALUE;
+        System.out.println("Byte Max Value is ="+byteMax);
+        byte byteMin = Byte.MIN_VALUE;
+        System.out.println("Byte Min Value is ="+byteMin);
+        System.out.println();
+
+        int intMax = Integer.MAX_VALUE;
+        System.out.println("Integer Max Value is ="+intMax);
+        int intMin = Integer.MIN_VALUE;
+        System.out.println("Integer Min Value is ="+intMin);
+        System.out.println();
+
+        int intMaxtest= intMax + 1;
+        int intMintest = intMin +1;
+        System.out.println("INT MAX TEST value is ="+intMaxtest);
+        System.out.println("INT MIN TEST value is ="+intMintest);
+
+        
+        float floatMax = Float.MAX_VALUE;
+        System.out.println("Float Max Value is ="+floatMax);
+        float floatMin = Float.MIN_VALUE;
+        System.out.println("Float Min Value is ="+floatMin);
+        System.out.println();
+        
+        long longMax = Long.MAX_VALUE;
+        System.out.println("Long Max Value is ="+longMax);
+        long longMin = Long.MIN_VALUE;
+        System.out.println("Long Min Value is ="+longMin);
+        System.out.println();
+
+        short shortMax = Short.MAX_VALUE;
+        System.out.println("Short Max Value is ="+shortMax);
+        short shortMin = Short.MIN_VALUE;
+        System.out.println("Short Min Value is ="+shortMin);
+        System.out.println();
+        
+
+        char charMax = Character.MAX_VALUE;
+        System.out.println("Byte Max Value is ="+charMax);
+        char charMin = Character.MIN_VALUE;
+        System.out.println("Byte Min Value is ="+charMin);
+        System.out.println();
+
+        double doubleMax = Double.MAX_VALUE;
+        System.out.println("DOUBLE Max Value is ="+doubleMax);
+        double doubleMin = Double.MIN_VALUE;
+        System.out.println("DOUBLE Min Value is ="+doubleMin);
+        System.out.println();
+
+
+
+    }
+}
