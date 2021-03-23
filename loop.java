87ee85665efa93bc77656a871215f0a6f583648a
Tonmoy dey <tonmoyd2001@gmail.com>
2021-03-23 22:07:38 +0600

    -35

---
+++ loop.java
@@ -0,0 +1,15 @@
+public class loop {
+    public static void main(String[] args) {
+        for(int i=0;;i--) {
+
+         if (i==-35){
+             System.out.println(i);
+             System.out.println("stop");
+             break;
+         }
+            System.out.print(i);
+        }
+
+    }
+}
+
