42b188885ba3973c2e7f95cd9f7aa29c78cba99d
Tonmoy dey <tonmoyd2001@gmail.com>
2021-03-23 23:52:59 +0600

    first and sum

---
+++ Num.java
@@ -0,0 +1,10 @@
+public class Num {
+    public static void main(String[] args) {
+        int y=4567;int i =10;
+        int sum ;
+        sum = i*(i+1)/2;
+        int lastDigits =y%10;
+        System.out.println(lastDigits);
+        System.out.println("sum = " + sum);
+    }
+}
