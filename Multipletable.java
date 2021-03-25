07d2d1b12a66071a44c518d5ee51050059e40d8f
Tonmoy dey <tonmoyd2001@gmail.com>
2021-03-25 22:35:19 +0600

    Table

---
+++ Multipletable.java
@@ -0,0 +1,10 @@
+public class Multipletable {
+    public static void main(String[] args) {
+        int num=2;
+        for(int i =1;i<=10;i++){
+            System.out.print(num + "*");
+            System.out.print(i + "=");
+            System.out.println( num * i);
+        }
+    }
+}
