fdda60a20c34b89eca2def98d7b5d635ab77b9fe
Tonmoy dey <tonmoyd2001@gmail.com>
2021-03-31 00:53:34 +0600

    Tonmoy dey

---
+++ Human.java
@@ -0,0 +1,10 @@
+public class Human {
+    public Human(String name) {
+        System.out.println("good morning -" + name);
+    }
+
+    public static void main(String[] args) {
+        Human obj1= new Human("Tonmoy Dey");
+
+    }
+}
